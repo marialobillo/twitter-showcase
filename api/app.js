a18259dbdb44e5df1c02f457d26e55513c9de3af
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-01-14 20:25:54 +0100

    api done

--- api/app.js
+++ api/app.js
@@ -1,7 +1,4 @@
 const Twit = require('twit');
-//const axios = require('axios');
-const fetch = require('cross-fetch');
-
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 9000;
@@ -62,12 +59,41 @@ function respondSearch(req, res){
    
 }
 
-function respondRandom(){
+function respondUserTwetts(req, res){
+    const { input = ''} = req.query;
+    var options = { screen_name: input,
+                count: 20 };
+    const datasearch = [];
+    T.get('statuses/user_timeline', options , (err, data) => {
+        data.map(twett => {
+            let datatwett = {};
+            datatwett.profile_image_url = twett.user.profile_image_url;
+            datatwett.screen_name = twett.user.screen_name;
+            datatwett.name = twett.user.name;
+            datatwett.text = twett.text;
+            datatwett.created_at = twett.created_at;
+            datatwett.retwett_count = twett.retweet_count;
+            datatwett.favorite_count = twett.favorite_count;
 
+            datasearch.push(datatwett);
+        })
+        let randomNumber = Math.floor(Math.random() * 20);
+        res.json(datasearch[randomNumber]);
+      })
 }
 
+/*
+FOR SEARCH: localhost:9000/search/?input=input
+FOR USERS: localhost:9000/uses/?input=screen_name
+brendaneich
+_ericelliot
+LeaVerou
+dan_abramov
+kentcdodds
+*/
+
 app.get('/search/*', respondSearch);
-//app.get('/random', respondRandom);
+app.get('/users/*', respondUserTwetts);
 
 app.listen(port, 
     () => console.log(`Server listening on port ${port}`));
